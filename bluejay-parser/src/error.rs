7d09f81aa42be3f2569a9e4d9fedf6577e8a299e
Adam Petro <adam.petro@shopify.com>
2023-08-03 16:32:06 -0400

    Add utility struct for converting spans to locations

--- bluejay-parser/src/error.rs
+++ bluejay-parser/src/error.rs
@@ -3,8 +3,12 @@ use ariadne::{Config, Label, Report, ReportKind, Source};
 use std::borrow::Cow;
 
 mod annotation;
+#[cfg(feature = "format-errors")]
+mod format_errors;
 
 pub use annotation::Annotation;
+#[cfg(feature = "format-errors")]
+pub use format_errors::SpanToLocation;
 
 #[derive(Debug, PartialEq)]
 pub struct Error {
@@ -32,7 +36,7 @@ impl Error {
         errors: impl IntoIterator<Item = E>,
     ) -> String {
         let mut file_cache = Source::from(document);
-        let mut byte_idx_to_char_idx = ByteIndexToCharIndex::new(document);
+        let mut byte_idx_to_char_idx = format_errors::ByteIndexToCharIndex::new(document);
 
         let mut buf: Vec<u8> = Vec::new();
 
@@ -74,54 +78,5 @@ impl Error {
 
     pub fn message(&self) -> &str {
         self.message.as_ref()
-    }
-}
-
-struct Index {
-    byte: usize,
-    char: usize,
-}
-
-/// `ariadne` uses char indices, but `logos` uses byte indices.
-/// This struct converts byte indices to char indices.
-struct ByteIndexToCharIndex<'a> {
-    document: &'a str,
-    mapping: Vec<Index>,
-}
-
-impl<'a> ByteIndexToCharIndex<'a> {
-    fn new(document: &'a str) -> Self {
-        Self {
-            document,
-            mapping: vec![Index { byte: 0, char: 0 }],
-        }
-    }
-
-    fn convert(&mut self, byte_idx: usize) -> usize {
-        match self
-            .mapping
-            .binary_search_by_key(&byte_idx, |index| index.byte)
-        {
-            Ok(idx) => self.mapping[idx].char,
-            Err(idx) => {
-                let char_idx = self.mapping[idx - 1].char
-                    + self.document[self.mapping[idx - 1].byte..byte_idx]
-                        .chars()
-                        .count();
-                self.mapping.insert(
-                    idx,
-                    Index {
-                        byte: byte_idx,
-                        char: char_idx,
-                    },
-                );
-                char_idx
-            }
-        }
-    }
-
-    fn convert_span(&mut self, span: &crate::Span) -> std::ops::Range<usize> {
-        let span = span.byte_range();
-        self.convert(span.start)..self.convert(span.end)
     }
 }
---
+++ bluejay-parser/src/error/format_errors.rs
@@ -0,0 +1,85 @@
+use ariadne::Source;
+
+struct Index {
+    byte: usize,
+    char: usize,
+}
+
+/// `ariadne` uses char indices, but `logos` uses byte indices.
+/// This struct converts byte indices to char indices.
+pub(crate) struct ByteIndexToCharIndex<'a> {
+    document: &'a str,
+    mapping: Vec<Index>,
+}
+
+impl<'a> ByteIndexToCharIndex<'a> {
+    pub(crate) fn new(document: &'a str) -> Self {
+        Self {
+            document,
+            mapping: vec![Index { byte: 0, char: 0 }],
+        }
+    }
+
+    pub(crate) fn convert(&mut self, byte_idx: usize) -> usize {
+        match self
+            .mapping
+            .binary_search_by_key(&byte_idx, |index| index.byte)
+        {
+            Ok(idx) => self.mapping[idx].char,
+            Err(idx) => {
+                let char_idx = self.mapping[idx - 1].char
+                    + self.document[self.mapping[idx - 1].byte..byte_idx]
+                        .chars()
+                        .count();
+                self.mapping.insert(
+                    idx,
+                    Index {
+                        byte: byte_idx,
+                        char: char_idx,
+                    },
+                );
+                char_idx
+            }
+        }
+    }
+
+    pub(crate) fn convert_span(&mut self, span: &crate::Span) -> std::ops::Range<usize> {
+        let span = span.byte_range();
+        self.convert(span.start)..self.convert(span.end)
+    }
+}
+
+pub struct SpanToLocation<'a> {
+    byte_idx_to_char_idx: ByteIndexToCharIndex<'a>,
+    source: Source,
+}
+
+impl<'a> SpanToLocation<'a> {
+    pub fn new(s: &'a str) -> Self {
+        Self {
+            byte_idx_to_char_idx: ByteIndexToCharIndex::new(s),
+            source: Source::from(s),
+        }
+    }
+
+    pub fn convert(&mut self, span: &crate::Span) -> Option<(usize, usize)> {
+        let span = span.byte_range();
+        let start = self.byte_idx_to_char_idx.convert(span.start);
+        self.source
+            .get_offset_line(start)
+            .map(|(_, line, col)| (line + 1, col + 1))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SpanToLocation;
+    use crate::Span;
+
+    #[test]
+    fn test_span_to_location() {
+        let mut span_to_location = SpanToLocation::new("hello\r\nworld");
+        assert_eq!(span_to_location.convert(&Span::new(0..5)), Some((1, 1)));
+        assert_eq!(span_to_location.convert(&Span::new(7..12)), Some((2, 1)));
+    }
+}
