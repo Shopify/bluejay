80afd1bdc70ea4a4ae67328b52b34a3e6cbac154
Adam Petro <adam.petro@shopify.com>
2023-09-28 11:48:04 -0400

    Remove base input/output types in visibility crate

--- bluejay-visibility/src/output_type.rs
+++ bluejay-visibility/src/output_type.rs
@@ -6,22 +6,28 @@ use bluejay_core::definition::{
     self, prelude::*, BaseOutputTypeReference, OutputTypeReference, SchemaDefinition,
     TypeDefinitionReference,
 };
-use bluejay_core::BuiltinScalarDefinition;
 
-pub enum BaseOutputType<'a, S: SchemaDefinition, W: Warden<SchemaDefinition = S>> {
-    BuiltinScalar(BuiltinScalarDefinition),
-    CustomScalar(&'a ScalarTypeDefinition<'a, S, W>),
-    Object(&'a ObjectTypeDefinition<'a, S, W>),
-    Interface(&'a InterfaceTypeDefinition<'a, S, W>),
-    Enum(&'a EnumTypeDefinition<'a, S, W>),
-    Union(&'a UnionTypeDefinition<'a, S, W>),
+pub enum OutputType<'a, S: SchemaDefinition + 'a, W: Warden<SchemaDefinition = S> + 'a> {
+    Base(BaseOutputTypeReference<'a, Self>, bool),
+    List(Box<Self>, bool),
 }
 
-impl<'a, S: SchemaDefinition + 'a, W: Warden<SchemaDefinition = S>> BaseOutputType<'a, S, W> {
-    pub(crate) fn new(
+impl<'a, S: SchemaDefinition + 'a, W: Warden<SchemaDefinition = S>> OutputType<'a, S, W> {
+    pub(crate) fn new(inner: &'a S::OutputType, cache: &'a Cache<'a, S, W>) -> Option<Self> {
+        match inner.as_ref() {
+            OutputTypeReference::Base(b, required) => {
+                Self::base(b, cache).map(|base| Self::Base(base, required))
+            }
+            OutputTypeReference::List(inner, required) => {
+                Self::new(inner, cache).map(|inner| Self::List(Box::new(inner), required))
+            }
+        }
+    }
+
+    fn base(
         inner: BaseOutputTypeReference<'a, S::OutputType>,
         cache: &'a Cache<'a, S, W>,
-    ) -> Option<Self> {
+    ) -> Option<BaseOutputTypeReference<'a, Self>> {
         let tdr = match inner {
             BaseOutputTypeReference::BuiltinScalar(bstd) => {
                 TypeDefinitionReference::BuiltinScalar(bstd)
@@ -38,49 +44,18 @@ impl<'a, S: SchemaDefinition + 'a, W: Warden<SchemaDefinition = S>> BaseOutputTy
         cache
             .get_or_create_type_definition(tdr)
             .map(|type_definition| match type_definition {
-                TypeDefinition::BuiltinScalar(bstd) => Self::BuiltinScalar(*bstd),
-                TypeDefinition::CustomScalar(cstd) => Self::CustomScalar(cstd),
-                TypeDefinition::Enum(etd) => Self::Enum(etd),
-                TypeDefinition::Interface(itd) => Self::Interface(itd),
-                TypeDefinition::Object(otd) => Self::Object(otd),
-                TypeDefinition::Union(utd) => Self::Union(utd),
+                TypeDefinition::BuiltinScalar(bstd) => {
+                    BaseOutputTypeReference::BuiltinScalar(*bstd)
+                }
+                TypeDefinition::CustomScalar(cstd) => BaseOutputTypeReference::CustomScalar(cstd),
+                TypeDefinition::Enum(etd) => BaseOutputTypeReference::Enum(etd),
+                TypeDefinition::Interface(itd) => BaseOutputTypeReference::Interface(itd),
+                TypeDefinition::Object(otd) => BaseOutputTypeReference::Object(otd),
+                TypeDefinition::Union(utd) => BaseOutputTypeReference::Union(utd),
                 TypeDefinition::InputObject(_) => {
                     panic!("Schema definition does not have unique type names");
                 }
             })
-    }
-}
-
-impl<'a, S: SchemaDefinition, W: Warden<SchemaDefinition = S>> From<&BaseOutputType<'a, S, W>>
-    for BaseOutputTypeReference<'a, OutputType<'a, S, W>>
-{
-    fn from(value: &BaseOutputType<'a, S, W>) -> Self {
-        match value {
-            BaseOutputType::BuiltinScalar(bstd) => BaseOutputTypeReference::BuiltinScalar(*bstd),
-            BaseOutputType::CustomScalar(cstd) => BaseOutputTypeReference::CustomScalar(*cstd),
-            BaseOutputType::Enum(etd) => BaseOutputTypeReference::Enum(*etd),
-            BaseOutputType::Interface(itd) => BaseOutputTypeReference::Interface(*itd),
-            BaseOutputType::Object(otd) => BaseOutputTypeReference::Object(*otd),
-            BaseOutputType::Union(utd) => BaseOutputTypeReference::Union(*utd),
-        }
-    }
-}
-
-pub enum OutputType<'a, S: SchemaDefinition, W: Warden<SchemaDefinition = S>> {
-    Base(BaseOutputType<'a, S, W>, bool),
-    List(Box<Self>, bool),
-}
-
-impl<'a, S: SchemaDefinition + 'a, W: Warden<SchemaDefinition = S>> OutputType<'a, S, W> {
-    pub(crate) fn new(inner: &'a S::OutputType, cache: &'a Cache<'a, S, W>) -> Option<Self> {
-        match inner.as_ref() {
-            OutputTypeReference::Base(b, required) => {
-                BaseOutputType::new(b, cache).map(|base| Self::Base(base, required))
-            }
-            OutputTypeReference::List(inner, required) => {
-                Self::new(inner, cache).map(|inner| Self::List(Box::new(inner), required))
-            }
-        }
     }
 }
 
@@ -95,7 +70,7 @@ impl<'a, S: SchemaDefinition + 'a, W: Warden<SchemaDefinition = S>> definition::
 
     fn as_ref(&self) -> OutputTypeReference<'_, Self> {
         match self {
-            Self::Base(b, required) => OutputTypeReference::Base(b.into(), *required),
+            Self::Base(b, required) => OutputTypeReference::Base(*b, *required),
             Self::List(inner, required) => OutputTypeReference::List(inner, *required),
         }
     }
