d7b602f084b25cdc6966520ef9cc69cd7d547401
Adam Petro <adam.petro@shopify.com>
2023-10-27 15:39:25 -0400

    Index fields definition in visibility crate

--- bluejay-visibility/src/fields_definition.rs
+++ bluejay-visibility/src/fields_definition.rs
@@ -1,12 +1,18 @@
 use crate::{Cache, FieldDefinition, Warden};
-use bluejay_core::definition::{self, SchemaDefinition};
+use bluejay_core::definition::{self, prelude::*, SchemaDefinition};
 use bluejay_core::AsIter;
+use elsa::FrozenMap;
 use once_cell::unsync::OnceCell;
+use std::rc::Rc;
+
+type IndexedFieldsDefinition<'a, S, W> =
+    FrozenMap<&'a str, Box<Option<Rc<FieldDefinition<'a, S, W>>>>>;
 
 pub struct FieldsDefinition<'a, S: SchemaDefinition, W: Warden<SchemaDefinition = S>> {
     inner: &'a S::FieldsDefinition,
     cache: &'a Cache<'a, S, W>,
-    fields_definition: OnceCell<Vec<FieldDefinition<'a, S, W>>>,
+    fields_definition: OnceCell<Vec<Rc<FieldDefinition<'a, S, W>>>>,
+    indexed_fields_definition: IndexedFieldsDefinition<'a, S, W>,
 }
 
 impl<'a, S: SchemaDefinition, W: Warden<SchemaDefinition = S>> FieldsDefinition<'a, S, W> {
@@ -15,6 +21,7 @@ impl<'a, S: SchemaDefinition, W: Warden<SchemaDefinition = S>> FieldsDefinition<
             inner,
             cache,
             fields_definition: OnceCell::new(),
+            indexed_fields_definition: Default::default(),
         }
     }
 }
@@ -23,17 +30,28 @@ impl<'a, S: SchemaDefinition + 'a, W: Warden<SchemaDefinition = S>> AsIter
     for FieldsDefinition<'a, S, W>
 {
     type Item = FieldDefinition<'a, S, W>;
-    type Iterator<'b> = std::slice::Iter<'b, Self::Item> where 'a: 'b;
+    type Iterator<'b> = std::iter::Map<std::slice::Iter<'b, Rc<Self::Item>>, fn(&'b Rc<Self::Item>) -> &'b Self::Item> where 'a: 'b;
 
     fn iter(&self) -> Self::Iterator<'_> {
         self.fields_definition
             .get_or_init(|| {
                 self.inner
                     .iter()
-                    .filter_map(|fd| FieldDefinition::new(fd, self.cache))
+                    .filter_map(|fd| {
+                        self.indexed_fields_definition
+                            .get(fd.name())
+                            .unwrap_or_else(|| {
+                                let scoped_fd = FieldDefinition::new(fd, self.cache).map(Rc::new);
+                                self.indexed_fields_definition
+                                    .insert(fd.name(), Box::new(scoped_fd))
+                            })
+                            .as_ref()
+                            .cloned()
+                    })
                     .collect()
             })
             .iter()
+            .map(std::ops::Deref::deref)
     }
 }
 
@@ -41,4 +59,23 @@ impl<'a, S: SchemaDefinition + 'a, W: Warden<SchemaDefinition = S>> definition::
     for FieldsDefinition<'a, S, W>
 {
     type FieldDefinition = FieldDefinition<'a, S, W>;
+
+    fn get(&self, name: &str) -> Option<&Self::FieldDefinition> {
+        self.indexed_fields_definition
+            .get(name)
+            .unwrap_or_else(|| {
+                if let Some(fd) = self.inner.get(name) {
+                    let scoped_fd = FieldDefinition::new(fd, self.cache).map(Rc::new);
+                    self.indexed_fields_definition
+                        .insert(fd.name(), Box::new(scoped_fd))
+                } else {
+                    &None
+                }
+            })
+            .as_deref()
+    }
+
+    fn contains_field(&self, name: &str) -> bool {
+        self.get(name).is_some()
+    }
 }
