290d9a147c918cf33c3bed7474b5d12a0ffca8f8
Adam Petro <adam.petro@shopify.com>
2024-05-22 11:03:56 -0400

    More typegen tests

--- bluejay-typegen/tests/miniserde_integration_test.rs
+++ bluejay-typegen/tests/miniserde_integration_test.rs
@@ -5,14 +5,24 @@ use mnsrd::json;
 
 #[typegen([
     type Query {
-        myNestedField: MyType
+        myType: MyType
         myUnion: MyUnion
+        myInterface: MyInterface
+        builtinScalars: BuiltinScalars
     }
-    type MyType {
+    type MyType implements MyInterface {
         myField: String!
+        myList: [String!]!
     }
     type MyOtherType {
         myOtherField: MyScalar!
+    }
+    type BuiltinScalars {
+        int: Int!
+        float: Float!
+        string: String!
+        boolean: Boolean!
+        id: ID!
     }
     union MyUnion = MyType | MyOtherType
     enum MyEnum {
@@ -27,15 +37,26 @@ use mnsrd::json;
         myField: String
         myCircularField: MyInput
     }
+    input BuiltinScalarsInput {
+        int: Int!
+        float: Float!
+        string: String!
+        boolean: Boolean!
+        id: ID!
+    }
     scalar MyScalar
+    interface MyInterface {
+        myField: String!
+    }
 ], codec = "miniserde")]
 mod schema {
     type MyScalar = String;
 
     #[query([
         query Object {
-            myNestedField {
+            myType {
                 myField
+                myList
             }
         }
 
@@ -48,6 +69,32 @@ mod schema {
                 ...on MyOtherType {
                     myOtherField
                 }
+            }
+        }
+
+        query Interface {
+            myInterface {
+                myField
+            }
+        }
+
+        query ObjectWithFragment {
+            myType {
+                ...MyFragment
+            }
+        }
+
+        fragment MyFragment on MyType {
+            myField
+        }
+
+        query BuiltinScalars {
+            builtinScalars {
+                int
+                float
+                string
+                boolean
+                id
             }
         }
     ])]
@@ -81,7 +128,7 @@ fn test_one_of_input_object() {
 }
 
 #[test]
-fn test_input_object() {
+fn test_circular_input_object() {
     let value = schema::MyInput {
         my_field: Some("hello".into()),
         my_circular_field: Some(Box::new(schema::MyInput {
@@ -96,13 +143,30 @@ fn test_input_object() {
 }
 
 #[test]
+fn test_builtin_scalars_input_object() {
+    let value = schema::BuiltinScalarsInput {
+        int: 1,
+        float: 1.2,
+        string: "hello".into(),
+        boolean: true,
+        id: "1".into(),
+    };
+    assert_eq!(
+        "{\"int\":1,\"float\":1.2,\"string\":\"hello\",\"boolean\":true,\"id\":\"1\"}",
+        json::to_string(&value)
+    );
+}
+
+#[test]
 fn test_deserialize_object() {
     let raw =
-        json::from_str("{\"myNestedField\":{\"myField\":\"hello\"}}").expect("Error parsing value");
+        json::from_str("{\"myType\":{\"myField\":\"hello\",\"myList\":[\"hello\",\"world\"]}}")
+            .expect("Error parsing value");
     assert_eq!(
         schema::query::Object {
-            my_nested_field: Some(schema::query::object::MyNestedField {
-                my_field: "hello".into()
+            my_type: Some(schema::query::object::MyType {
+                my_field: "hello".into(),
+                my_list: vec!["hello".into(), "world".into()],
             }),
         },
         raw
@@ -132,6 +196,53 @@ fn test_deserialize_union_other() {
     assert_eq!(
         schema::query::Union {
             my_union: Some(schema::query::r#union::MyUnion::Other),
+        },
+        raw
+    );
+}
+
+#[test]
+fn test_deserialize_interface() {
+    let raw =
+        json::from_str("{\"myInterface\":{\"myField\":\"hello\"}}").expect("Error parsing value");
+    assert_eq!(
+        schema::query::Interface {
+            my_interface: Some(schema::query::interface::MyInterface {
+                my_field: "hello".into()
+            }),
+        },
+        raw
+    );
+}
+
+#[test]
+fn test_deserialize_object_with_fragment() {
+    let raw = json::from_str("{\"myType\":{\"myField\":\"hello\"}}").expect("Error parsing value");
+    assert_eq!(
+        schema::query::ObjectWithFragment {
+            my_type: Some(schema::query::MyFragment {
+                my_field: "hello".into(),
+            }),
+        },
+        raw
+    );
+}
+
+#[test]
+fn test_deserialize_builtin_scalars() {
+    let raw = json::from_str(
+        "{\"builtinScalars\":{\"int\":1,\"float\":1.2,\"string\":\"hello\",\"boolean\":true,\"id\":\"1\"}}",
+    )
+    .expect("Error parsing value");
+    assert_eq!(
+        schema::query::BuiltinScalars {
+            builtin_scalars: Some(schema::query::builtin_scalars::BuiltinScalars {
+                int: 1,
+                float: 1.2,
+                string: "hello".into(),
+                boolean: true,
+                id: "1".into(),
+            }),
         },
         raw
     );
