d3971b7f10f5f2cb366c550afc8a6c336bf807c1
Adam Petro <adam.petro@shopify.com>
2024-06-26 11:36:46 -0400

    Document special directives

--- bluejay-operation-merger/src/lib.rs
+++ bluejay-operation-merger/src/lib.rs
@@ -3,7 +3,24 @@
 //! # Limitations
 //! There is no guarantee that the merged operation will be valid according to the GraphQL specification. The following limitations apply:
 //! - operation types of the same name must have the same operation type (query, mutation, or subscription)
-//! - directives are not supported
+//! - directives are not supported, with the exception of those mentioned below in [Supported directives](#supported-directives)
+//!
+//! # Supported directives
+//! The following directives are supported:
+//! ```graphql
+//! """
+//! Used to specify that a field should be aliased with a suffix, or a variable definition should have a suffix added to its name.
+//! The suffix is the value in the user provided context at `contextKey`.
+//! """
+//! directive @suffixOnMerge(contextKey: String!) on VARIABLE_DEFINITION | FIELD
+//!
+//! """
+//! Used to specify that a variable definition should be replaced with a value from the user provided context.
+//! The variable definition will not be present in the merged document, and any references to the variable
+//! will be replaced with the value in the user provided context at `contextKey`.
+//! """
+//! directive @replaceOnMerge(contextKey: String!) on VARIABLE_DEFINITION
+//! ```
 //!
 //! # Example
 //! ```
